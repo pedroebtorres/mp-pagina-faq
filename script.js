4e7c7757121875604a7a26ec3d4b6644194de551
pedroebtorres <pebtorres@hotmail.com>
2023-10-06 15:28:56 -0400

    Could be Better

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function showContent(index) {
     let temp = document.getElementsByTagName("template")[index];
     let clon = temp.content.cloneNode(true);
-    let div = document.getElementsByTagName("div")[index + 2];
+    let div = document.getElementsByTagName("div")[index + 3];
     console.log(div.children.length);
     if (div.children.length === 4) {
         let elementToRemove = div.querySelector('p');
